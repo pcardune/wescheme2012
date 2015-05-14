d7b67b6c7161035205bd37a28055451d40bd6745
Dorai Sitaram <ds26gte@yahoo.com>
2015-05-14 13:59:15 -0400

    set exports.roughnum = types.roughnum

--- war-src/js/js-runtime/index.js
+++ war-src/js/js-runtime/index.js
@@ -4,8 +4,6 @@ var primitive = require('./primitive');
 var jsnums = require('./js-numbers');
 var state = require('./state');
 
-
-
 exports.State = state.State;
 exports.Prefix = interpret.Prefix;
 exports.VariableReference = types.VariableReference;
@@ -16,11 +14,10 @@ exports.run = interpret.run;
 
 exports.setDebug = interpret.setDebug;
 
-
-
 exports.symbol = types.symbol;
 exports.keyword = types.keyword;
 exports.rational = types.rational;
+exports.roughnum = types.roughnum;
 exports['float'] = types['float'];
 exports.complex = types.complex;
 exports.pair = types.pair;
@@ -33,11 +30,8 @@ exports.box = types.box;
 exports.path = types.path;
 exports.bytes = types.bytes;
 
-
-
 exports.FALSE = Logic.FALSE;
 exports.TRUE = Logic.TRUE;
-
 
 exports.UNDEFINED = types.UNDEFINED;
 exports.VOID = types.VOID;
@@ -47,8 +41,5 @@ exports.EMPTY = types.EMPTY;
 
 exports.ClosureValue = types.ClosureValue;
 exports.Primitive = primitive.Primitive;
-
-
-
 
 exports.lessThanOrEqual = jsnums.lessThanOrEqual;
